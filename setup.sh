12ab74d3356ec233d4da74f40085c7ad7a99ec43
H6rst0n <104990977+H6rst0n@users.noreply.github.com>
2025-01-20 05:28:27 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 echo "source $PWD/.tht_env" >> ~/.bashrc
-mkdir 1-1.water_opt 1-2.water_npt 1-3.water_nvt
+mkdir 1-1.water_opt 1-2.water_npt 1-3.water_nvt 2.ReaxFF
+mv cis_polyacetylene.xyz 2.ReaxFF
